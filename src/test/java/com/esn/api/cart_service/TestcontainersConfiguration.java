63ec88c2d936a8c40d66970b90c16f3769bcd067
indrajitbastapure@gmail.com <indrajitbastapure@gmail.com>
2024-12-09 17:31:03 +0100

    first commit

---
+++ src/test/java/com/esn/api/cart_service/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.esn.api.cart_service;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
